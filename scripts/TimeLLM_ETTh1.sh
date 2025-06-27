8b2bd4853b32ecdda9fdf7ac31f7a1d489aa148b
Donald <donald.bleyl@gmail.com>
2025-06-27 18:07:40 -0400

    chore: Add .gitignore and local accelerate config for TimeLLM_ETTh1.sh
    - Add a gitignore to ignore additional datasets and non-versioned files
    - Configure TimeLLM_ETTh1.sh to support a local accelerate config

--- scripts/TimeLLM_ETTh1.sh
+++ scripts/TimeLLM_ETTh1.sh
@@ -3,15 +3,17 @@ train_epochs=100
 learning_rate=0.01
 llama_layers=32
 
-master_port=00097
-num_process=8
+# master_port=00097
+master_port=29500
+# num_process=8
+num_process=2
 batch_size=24
 d_model=32
 d_ff=128
 
 comment='TimeLLM-ETTh1'
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+accelerate launch --config_file ./accelerate_config.yaml --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
@@ -37,7 +39,7 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --train_epochs $train_epochs \
   --model_comment $comment
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+accelerate launch --config_file ./accelerate_config.yaml --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
@@ -63,7 +65,7 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --train_epochs $train_epochs \
   --model_comment $comment
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+accelerate launch --config_file ./accelerate_config.yaml --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
@@ -90,7 +92,7 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --train_epochs $train_epochs \
   --model_comment $comment
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+accelerate launch --config_file ./accelerate_config.yaml --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/ETT-small/ \
